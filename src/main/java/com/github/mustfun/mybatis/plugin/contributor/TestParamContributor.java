6e12185285bc05d0a61ceb30b9afde61d1cc5408
dengzhiyuan <dengzhiyuan@hujiang.com>
2018-06-29 11:27:14 +0800

    update: 加入少许注释v6

--- src/main/java/com/github/mustfun/mybatis/plugin/contributor/TestParamContributor.java
+++ src/main/java/com/github/mustfun/mybatis/plugin/contributor/TestParamContributor.java
@@ -23,29 +23,29 @@ import org.jetbrains.annotations.Nullable;
  */
 public class TestParamContributor extends CompletionContributor {
 
-  @SuppressWarnings("unchecked")
-  public TestParamContributor() {
-    extend(CompletionType.BASIC,
-           XmlPatterns.psiElement().inside(XmlPatterns.xmlAttributeValue().inside(XmlPatterns.xmlAttribute().withName("test"))),
-           new CompletionProvider<CompletionParameters>() {
-              @Override
-              protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
-                PsiElement position = parameters.getPosition();
-                addElementForPsiParameter(position.getProject(), result, MapperUtils.findParentIdDomElement(position).orNull());
-              }
-    });
-  }
+    @SuppressWarnings("unchecked")
+    public TestParamContributor() {
+        extend(CompletionType.BASIC,
+                XmlPatterns.psiElement().inside(XmlPatterns.xmlAttributeValue().inside(XmlPatterns.xmlAttribute().withName("test"))),
+                new CompletionProvider<CompletionParameters>() {
+                    @Override
+                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
+                        PsiElement position = parameters.getPosition();
+                        addElementForPsiParameter(position.getProject(), result, MapperUtils.findParentIdDomElement(position).orNull());
+                    }
+                });
+    }
 
-  public static void addElementForPsiParameter(@NotNull Project project, @NotNull CompletionResultSet result, @Nullable IdDomElement element) {
-    if (null == element) {
-      return;
+    public static void addElementForPsiParameter(@NotNull Project project, @NotNull CompletionResultSet result, @Nullable IdDomElement element) {
+        if (null == element) {
+            return;
+        }
+        for (PsiParameter parameter : JavaUtils.findMethod(project, element).get().getParameterList().getParameters()) {
+            Optional<String> valueText = JavaUtils.getAnnotationValueText(parameter, Annotation.PARAM);
+            if (valueText.isPresent()) {
+                LookupElementBuilder builder = LookupElementBuilder.create(valueText.get()).withIcon(Icons.PARAM_COMPLETION_ICON);
+                result.addElement(PrioritizedLookupElement.withPriority(builder, MybatisConstants.PRIORITY));
+            }
+        }
     }
-    for (PsiParameter parameter : JavaUtils.findMethod(project, element).get().getParameterList().getParameters()) {
-      Optional<String> valueText = JavaUtils.getAnnotationValueText(parameter, Annotation.PARAM);
-      if (valueText.isPresent()) {
-        LookupElementBuilder builder = LookupElementBuilder.create(valueText.get()).withIcon(Icons.PARAM_COMPLETION_ICON);
-        result.addElement(PrioritizedLookupElement.withPriority(builder, MybatisConstants.PRIORITY));
-      }
-    }
-  }
 }
