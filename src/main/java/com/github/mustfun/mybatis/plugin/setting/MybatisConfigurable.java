763d00c9652b2e513fdc1287712bd673fef42ea9
dengzhiyuan <zhiyuan_deng@sui.com>
2018-08-22 20:22:09 +0800

    update: update to 1.04, fix some bug

--- src/main/java/com/github/mustfun/mybatis/plugin/setting/MybatisConfigurable.java
+++ src/main/java/com/github/mustfun/mybatis/plugin/setting/MybatisConfigurable.java
@@ -40,7 +40,7 @@ public class MybatisConfigurable implements SearchableConfigurable {
 
     @Override
     public String getId() {
-        return "Mybatis";
+        return "Mybatis Plugin Free";
     }
 
     @Override
