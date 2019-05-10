dd97a11e100d027f2b21e58d56adfba89ac245d3
chenlong3 <chenlong3@360.cn>
2019-05-10 16:19:20 +0800

    fixbug:issues#31

--- src/main/java/com/github/mustfun/mybatis/plugin/util/DbUtil.java
+++ src/main/java/com/github/mustfun/mybatis/plugin/util/DbUtil.java
@@ -1,6 +1,10 @@
 package com.github.mustfun.mybatis.plugin.util;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,27 +17,31 @@ import java.util.Properties;
  * @since 1.0
  */
 public class DbUtil {
-    private  String url;
-    private  String username;
-    private  String password;
 
-    public DbUtil(String url, String dbName, String userName, String password){
-        this.url = "jdbc:mysql://"+url+":3306/"+dbName+"?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false";
+    private String url;
+    private String username;
+    private String password;
+
+    public DbUtil(String url, String dbName, String userName, String password) {
+        this.url = "jdbc:mysql://" + url + ":3306/" + dbName
+            + "?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&serverTimezone=GMT";
         this.username = userName;
         this.password = password;
     }
 
     /**
      * 增加端口配置
+     *
      * @date 2019/1/22
      */
-    public DbUtil(String url, String dbName, String userName, String password, int port){
-        this.url = "jdbc:mysql://"+url+":"+port+"/"+dbName+"?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false";
+    public DbUtil(String url, String dbName, String userName, String password, int port) {
+        this.url = "jdbc:mysql://" + url + ":" + port + "/" + dbName
+            + "?useUnicode=true&characterEncoding=utf8&autoReconnect=true&failOverReadOnly=false&serverTimezone=GMT";
         this.username = userName;
         this.password = password;
     }
 
-    public DbUtil(){
+    public DbUtil() {
 
     }
 
@@ -41,11 +49,11 @@ public class DbUtil {
     public Connection getConnection() {
         Connection conn = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        Properties props =new Properties();
+        Properties props = new Properties();
         props.setProperty("user", username);
         props.setProperty("password", password);
         //设置可以获取remarks信息
@@ -64,7 +72,7 @@ public class DbUtil {
      *
      * @return
      */
-    public Connection getSqlliteConnection(){
+    public Connection getSqlliteConnection() {
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
@@ -75,13 +83,13 @@ public class DbUtil {
             //String realPath = PathManager.getPluginsPath();
             // File path = PluginManager.getPlugin(PluginId.getId("mybatis-plugin-free")).getPath();
             //查看是否存在db文件，不存在就创建，存在就直接连
-            String tempPath = MybatisConstants.TEMP_DIR_PATH+"/db/generate_web.db";
+            String tempPath = MybatisConstants.TEMP_DIR_PATH + "/db/generate_web.db";
             File tempPathFile = new File(tempPath);
-            if (!tempPathFile.exists()){
+            if (!tempPathFile.exists()) {
                 InputStream s = DbUtil.this.getClass().getResourceAsStream("/db/generate_web.db");
                 tempPath = createCustomFontUrl(s);
             }
-            conn = DriverManager.getConnection("jdbc:sqlite:"+tempPath);
+            conn = DriverManager.getConnection("jdbc:sqlite:" + tempPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -90,17 +98,15 @@ public class DbUtil {
 
     /**
      * 将里面的数据库文件转移出来,非常巧妙,非常非常,终于解决难点OK
-     * @param stream
-     * @return
      */
     protected String createCustomFontUrl(InputStream stream) {
         String tempDirPath = MybatisConstants.TEMP_DIR_PATH;
         try {
             File tempFile = new File(tempDirPath + "/db");
-            if (!tempFile.exists()){
+            if (!tempFile.exists()) {
                 tempFile.mkdirs();
             }
-            File targetFile = new File(tempFile,"generate_web.db");
+            File targetFile = new File(tempFile, "generate_web.db");
             System.out.println("creating temp db File file: " + targetFile.getAbsolutePath());
 
             FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
