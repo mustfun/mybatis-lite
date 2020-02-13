8d9d548cdb82bced4da1003dc96414c4be9026b1
itar <itar@gitee.com>
2020-02-13 13:47:29 +0800

    update: 加入单元测试类

---
+++ src/main/test/com/github/mustfun/mybatis/plugin/test/GenerateXMLCodeTest.java
@@ -0,0 +1,13 @@
+package com.github.mustfun.mybatis.plugin.test;
+
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.junit.Test;
+
+public class GenerateXMLCodeTest extends BasePlatformTestCase {
+
+    @Test
+    public void testSome(){
+        PsiFile availableIntentions = myFixture.getFile();
+    }
+}
