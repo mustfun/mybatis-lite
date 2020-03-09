9e5b73d1c46fd4e8907fa183fd05a2fa0f952b65
itar <itar@gitee.com>
2020-03-09 18:13:39 +0800

    #55 加入模板初始化功能

--- src/main/java/com/github/mustfun/mybatis/plugin/action/TemplateEditMenuAction.java
+++ src/main/java/com/github/mustfun/mybatis/plugin/action/TemplateEditMenuAction.java
@@ -8,7 +8,6 @@ import com.github.mustfun.mybatis.plugin.setting.TemplateListForm;
 import com.github.mustfun.mybatis.plugin.setting.TemplateListForm.MyTableModel;
 import com.github.mustfun.mybatis.plugin.ui.custom.TemplateListPanel;
 import com.github.mustfun.mybatis.plugin.util.ConnectionHolder;
-import com.github.mustfun.mybatis.plugin.util.DbUtil;
 import com.github.mustfun.mybatis.plugin.util.Icons;
 import com.github.mustfun.mybatis.plugin.util.MybatisConstants;
 import com.intellij.AppTopics;
@@ -16,7 +15,6 @@ import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
@@ -36,17 +34,14 @@ import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumnModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author itar
@@ -56,14 +51,12 @@ import java.util.Objects;
  */
 public class TemplateEditMenuAction extends AnAction {
 
-    private static Template editingTemplate = new Template();
-
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
         TemplateListForm templateListForm = new TemplateListForm(project);
         JBTable templateList = templateListForm.getTemplateList();
-        String[] headName = {"模板名称", "创建人", "模板类型", "操作",""};
+        String[] headName = {"模板ID","模板名称", "创建人", "模板类型", "操作",""};
 
         SqlLiteService sqlLiteService = DbServiceFactory.getInstance(Objects.requireNonNull(project)).createSqlLiteService();
 
@@ -73,12 +66,13 @@ public class TemplateEditMenuAction extends AnAction {
             Template template = templates.get(i);
             JButton button = new JButton("编辑");
             JButton button2 = new JButton("初始化");
-            Object[] objects = new Object[5];
-            objects[0] = template.getTepName();
-            objects[1] = template.getCreateBy() == null ? "" : template.getCreateBy();
-            objects[2] = VmTypeEnums.findVmNameByVmType(template.getVmType()).getMgs();
-            objects[3] = button;
-            objects[4] = button2;
+            Object[] objects = new Object[6];
+            objects[0] = template.getId();
+            objects[1] = template.getTepName();
+            objects[2] = template.getCreateBy() == null ? "" : template.getCreateBy();
+            objects[3] = VmTypeEnums.findVmNameByVmType(template.getVmType()).getMgs();
+            objects[4] = button;
+            objects[5] = button2;
             obj[i] = objects;
         }
         templateList.setModel(new MyTableModel(headName, obj));
@@ -97,10 +91,15 @@ public class TemplateEditMenuAction extends AnAction {
             public void mouseClicked(MouseEvent e) {
                 int row = table.getSelectedRow();
                 int column = table.getSelectedColumn();
-                if (column == 3) {
+                if (column == 4) {
                     //处理button事件写在这里...
-                    String tepName = (String) table.getValueAt(row, 0);
-                    editingTemplate = templates.stream().filter(x -> x.getTepName().equals(tepName)).findAny().get();
+                    Integer id = (Integer) table.getValueAt(row, 0);
+                    Template editingTemplate = DbServiceFactory.getInstance(project).createSqlLiteService().queryTemplateById(id);
+                    if (editingTemplate==null){
+                        return ;
+                    }
+                    AtomicReference<PsiDirectory> psiDirectory = new AtomicReference<>();
+                    AtomicReference<PsiFile> file = new AtomicReference<>();
                     ApplicationManager.getApplication().runWriteAction(() -> {
                         //先保存，再打开，再删除
                         VirtualFile vFile = null;
@@ -112,13 +111,14 @@ public class TemplateEditMenuAction extends AnAction {
                         PsiFile psiFile = PsiFileFactory.getInstance(project)
                             .createFileFromText(editingTemplate.getTepName() + ".vm", JavaFileType.INSTANCE,
                                 editingTemplate.getTepContent().replaceAll("\r\n", "\n"));
-                        PsiDirectory psiDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(vFile);
-                        PsiFile file = psiDirectory.findFile(editingTemplate.getTepName() + ".vm");
-                        if (file == null) {
-                            psiDirectory.add(psiFile);
+                        psiDirectory.set(PsiDirectoryFactory.getInstance(project).createDirectory(vFile));
+                        file.set(psiDirectory.get().findFile(editingTemplate.getTepName() + ".vm"));
+                        if (file.get() != null) {
+                            psiDirectory.get().delete();
                         }
+                        psiDirectory.get().add(psiFile);
                         //再打开
-                        PsiFile realPsiFile = Arrays.stream(psiDirectory.getFiles())
+                        PsiFile realPsiFile = Arrays.stream(psiDirectory.get().getFiles())
                             .filter(x -> x.getName().equals(editingTemplate.getTepName() + ".vm")).findAny().get();
                         new OpenFileDescriptor(project, realPsiFile.getVirtualFile()).navigateInEditor(project, true);
 
@@ -133,9 +133,6 @@ public class TemplateEditMenuAction extends AnAction {
 
                             @Override
                             public void beforeDocumentSaving(@NotNull final Document document) {
-                                if (editingTemplate == null) {
-                                    return;
-                                }
                                 final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
 
                                 if (openProjects.length > 0) {
@@ -168,23 +165,18 @@ public class TemplateEditMenuAction extends AnAction {
                                     updatePo.setId(editingTemplate.getId());
                                     updatePo.setTepContent(text);
                                     instance.updateTemplate(updatePo);
-
                                 }
                             }
                         });
                 }
                 //还原模板文件用
-                if (column==4){
+                if (column==5){
                     if (Messages.showYesNoDialog("确定要恢复当前模板吗?", "Mybatis Lite", Icons.MYBATIS_LOGO_MINI)==Messages.NO){
                         return;
                     }
-                    String tepName = (String)table.getValueAt(row, 0);
-                    Template template = templates.stream().filter(x -> x.getTepName().equals(tepName)).findAny().orElse(null);
-                    if (template==null){
-                        return ;
-                    }
+                    Integer id = (Integer)table.getValueAt(row, 0);
                     SqlLiteService innerSqlLiteService = DbServiceFactory.getInstance(project).createInnerSqlLiteService();
-                    template = innerSqlLiteService.queryTemplateById(template.getId());
+                    Template template = innerSqlLiteService.queryTemplateById(id);
                     SqlLiteService sqlLiteService = DbServiceFactory.getInstance(project).createSqlLiteService();
                     sqlLiteService.updateTemplate(template);
                     Messages.showMessageDialog("恢复模板成功", "Mybatis Lite", Icons.MYBATIS_LOGO_MINI);
