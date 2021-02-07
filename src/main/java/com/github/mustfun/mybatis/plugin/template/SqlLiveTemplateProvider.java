eabb5e3d882d822820be67b31d6623aab23687a3
itar.deng <itar.deng@ximalaya.com>
2021-02-07 15:17:43 +0800

    update: 去除无用api

--- src/main/java/com/github/mustfun/mybatis/plugin/template/SqlLiveTemplateProvider.java
+++ src/main/java/com/github/mustfun/mybatis/plugin/template/SqlLiveTemplateProvider.java
@@ -6,18 +6,18 @@ import org.jetbrains.annotations.Nullable;
 /**
  * @author yanglin
  */
-public class SqlLiveTemplateProvider implements DefaultLiveTemplatesProvider {
-
-    public static final String[] TEMPLATE_FILES = {"liveTemplates/sql"};
-
-    @Override
-    public String[] getDefaultLiveTemplateFiles() {
-        return TEMPLATE_FILES;
-    }
-
-    @Nullable
-    @Override
-    public String[] getHiddenLiveTemplateFiles() {
-        return null;
-    }
-}
+//public class SqlLiveTemplateProvider implements DefaultLiveTemplatesProvider {
+//
+//    public static final String[] TEMPLATE_FILES = {"liveTemplates/sql"};
+//
+//    @Override
+//    public String[] getDefaultLiveTemplateFiles() {
+//        return TEMPLATE_FILES;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public String[] getHiddenLiveTemplateFiles() {
+//        return null;
+//    }
+//}
